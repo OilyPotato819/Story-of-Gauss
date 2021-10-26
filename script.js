2d1380e0e041ddb55acfc6d19772af8c9e02990c
OilyPotato819 <o.schneider@share.epsb.ca>
2021-10-26 14:48:31 -0600

    s

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// Script.js
+// Story of Gauss
+
+// HTML Elements
+let calcBtn = document.getElementById("calc-btn");
+let sumOut = document.getElementById("sum-out");
+
+// Add Event Listenener
+calcBtn.addEventListener("click", calcSum);
+
+// Event Function
+function calcSum() {
+    // Sum integers 1 to 100
+    let total = 0;
+    for (let n = 1; n <= 100; n++) {
+        total = total + n;
+    }
+
+    // Output the Total
+    sumOut.innerHTML = total;
+}
