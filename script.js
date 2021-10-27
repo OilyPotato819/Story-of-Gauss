50f1454747c1c805f4994d6cef7d0a821dc1fed4
OilyPotato819 <o.schneider@share.epsb.ca>
2021-10-27 14:31:48 -0600

    done

--- script.js
+++ script.js
@@ -13,17 +13,13 @@ calcBtn.addEventListener("click", calcSum);
 // Event Function
 function calcSum() {
     // Sum integers 1 to 100
-    let total = 0;
-    let x = nIn.value
+    let n = +nIn.value
+    let x = n * (n + 1) / 2;
 
-    for (let n = 1; n <= x && x <= 10000000; n++) {
-        total += n;
-    }
-
-    // Outputs
-    if (x >= 2 && x <= 10000000) {
-        nOut.innerHTML = x;
-        sumOut.innerHTML = total;
+    // // Outputs
+    if (n >= 2 && n <= 9000000) {
+        nOut.innerHTML = n;
+        sumOut.innerHTML = x;
         invalidNum.innerHTML = ""
     } else {
         nOut.innerHTML = "?";
